8456064c27d8c7e003fe8a7a8e7a4df05e3b49c3
SunghoCha <tjdgh3725@gmail.com>
2024-06-22 15:20:30 +0900

    JPA활용2 - 조회용 샘플 데이터 세팅
     - DDL : create로 설정 후 성능 최적화 테스트용 샘플 데이터를 애플리케이션 시작시에 db에 insert하는 init()메서드 구현

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -2,12 +2,13 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
+@Builder
 @DiscriminatorValue("B")
 @Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class Book extends Item{
 
     private String author;
