259e1d381183b3a6076748083682057cc1d782ea
SunghoCha <tjdgh3725@gmail.com>
2024-08-11 14:12:06 +0900

    Querydsl - 정렬, 페이징 테스트

--- Querydsl_study/src/test/java/study/querydsl_study/QuerydslBasicTest.java
+++ Querydsl_study/src/test/java/study/querydsl_study/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl_study;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.groups.Tuple;
@@ -16,6 +17,7 @@ import study.querydsl_study.entity.Team;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl_study.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -76,13 +78,131 @@ public class QuerydslBasicTest {
 
         // when
         Member findMember = queryFactory
-                .select(qMember)
-                .from(qMember)
-                .where(qMember.userName.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.userName.eq("member1"))
                 .fetchOne();
 
         // then
         assertThat(findMember.getUserName()).isEqualTo("member1");
+    }
+
+    @Test
+    @DisplayName("검색 조건 쿼리")
+    void search() {
+        // given
+        // when
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.userName.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        // then
+        assertThat(findMember.getUserName()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("검색 조건 쿼리 and 대신 ',' 사용가능")
+    void searchAndParam() {
+        // given
+        // when
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.userName.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        // then
+        assertThat(findMember.getUserName()).isEqualTo("member1");
+        assertThat(findMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("fetch 테스트")
+    void resultFetch() {
+        // given
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        // 추천되지 않는방법. 성능이슈, 서브쿼리에서 사용 어려움 등..
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        // 추천되지 않는방법. 성능이슈, 서브쿼리에서 사용 어려움 등..
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+        // when
+
+        // then
+    }
+
+    /*
+        회원 정렬 순서
+        1. 회원 나이 내림차순(desc)
+        2. 회원 이름 올림차순(asc)
+        3. 만약 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+
+    @Test
+    @DisplayName("sort 테스트")
+    void sort() {
+        // given
+        em.persist(createMember(null, 101, null));
+        em.persist(createMember("member5", 101, null));
+        em.persist(createMember("member6", 102, null));
+        em.persist(createMember("member7", 102, null));
+        // when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.goe(100))
+                .orderBy(member.age.desc(), member.userName.asc().nullsLast())
+                .fetch();
+
+        // then
+        assertThat(result).hasSize(4)
+                .extracting(Member::getAge, Member::getUserName)
+                .containsExactly(
+                        Tuple.tuple(102, "member6"),
+                        Tuple.tuple(102, "member7"),
+                        Tuple.tuple(101, "member5"),
+                        Tuple.tuple(101, null)
+                );
+    }
+
+    @Test
+    @DisplayName("paging 테스트")
+    void paging() {
+        // given
+        // when
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .orderBy(member.userName.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        // then
+        assertThat(members).hasSize(2)
+                .extracting(Member::getUserName, Member::getAge)
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("member2", 20),
+                        Tuple.tuple("member3", 30)
+                );
     }
 
     private static Member createMember(String name, int age, Team team) {
